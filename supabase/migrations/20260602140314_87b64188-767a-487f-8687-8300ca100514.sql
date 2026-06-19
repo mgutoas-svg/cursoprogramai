5eb09094cb421df9f33a652771c0054a2fc73765
Mario Augusto <mgutoas@gmail.com>
2026-06-18 21:28:20 -0300

    Delete supabase directory

--- supabase/migrations/20260602140314_87b64188-767a-487f-8687-8300ca100514.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE public.demandas ADD COLUMN whatsapp_contato TEXT;
-
-UPDATE public.demandas SET whatsapp_contato = '' WHERE whatsapp_contato IS NULL;
-
-COMMENT ON COLUMN public.demandas.whatsapp_contato IS 'Número de WhatsApp para contato sobre a demanda';
--- supabase/migrations/20260602140835_abf73b64-7e7a-42cc-9af1-86dfb97ef404.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE public.demandas ADD COLUMN nome_responsavel TEXT;
-
-UPDATE public.demandas SET nome_responsavel = '' WHERE nome_responsavel IS NULL;
-
-COMMENT ON COLUMN public.demandas.nome_responsavel IS 'Nome do responsável pelo chamado no cliente';
